4c45f47d9167c9359225032a205da3b097bac2d1
Tom-Hansen <143756451+Tom-Hansen@users.noreply.github.com>
2023-11-08 15:38:34 -0500

    Update HW2.cpp

--- HW2.cpp
+++ HW2.cpp
@@ -1,4 +1,5 @@
-﻿// HW2.cpp : Defines the entry point for the application.
+﻿// HW2.cpp: Defines the entry point for the application
+// Working together with Tom Hansen and Kameron Metfcalf
 
 #include <iostream>
 #include <string>
@@ -11,18 +12,18 @@ class Plane
 protected:
 	double wait_time;
 private:
-	double pos;
-	double vel;
-	double distance;
-	double loiter_time;
-	bool at_SCE;
-	string origin, destination;
+	double pos; //position
+	double vel; //velocity
+	double distance; //distance
+	double loiter_time; //amount of time airplanes need for possible landings
+	bool at_SCE; //State College Airport
+	string origin, destination; //origin and destination
 public:
 
 	/*A constructor that takes in two strings “from” and “to” as input
 		arguments.
 		- A virtual deconstructor.
-		- A function named “operate” with a double variables “dt” as an
+		- A function named “operate” with a double variable “dt” as an
 		input, and return nothing.
 		- get function for “pos” variable.
 		- get functions for “vel” variable.
@@ -34,7 +35,7 @@ public:
 		- set functions for “loiter_time” variable.
 		- A function named “distance_to_SCE” that returns double.
 		- A virtual function named “time_on_ground”.
-		- A virtual function named “plane_type” that returns string.
+		- A virtual function named “plane_type” that returns a string.
 		- A static function named “draw_from_normal_dist” that takes in two
 		double variables(mean and standard deviation), and returns back a
 		random number from a normal distribution.*/
