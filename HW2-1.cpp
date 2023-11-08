ad6d73e0a5c184cf958b6a653c6ad8ddd8b17c16
IanThePineapple <53544283+IanThePineapple@users.noreply.github.com>
2023-11-08 16:36:11 -0500

    Add files via upload
    Q1 & 2

--- HW2-1.cpp
+++ HW2-1.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include "HW2.h"
+#include <random>
 
 using namespace std;
 
@@ -28,28 +29,78 @@ private:
 	double loiter_time;
 	bool at_SCE;
 	string origin, destination;
-public:
+	vector<Flight> flights;
 
-	/*A constructor that takes in two strings “from” and “to” as input
-		arguments.
-		- A virtual deconstructor.
-		- A function named “operate” with a double variables “dt” as an
-		input, and return nothing.
-		- get function for “pos” variable.
-		- get functions for “vel” variable.
-		- get functions for “loiter_time” variable.
-		- get function for “origin” variable.
-		- get function for “destination” variable.
-		- get function for “at_SCE” variable.
-		- set functions for “vel” variable.
-		- set functions for “loiter_time” variable.
-		- A function named “distance_to_SCE” that returns double.
-		- A virtual function named “time_on_ground”.
-		- A virtual function named “plane_type” that returns string.
-		- A static function named “draw_from_normal_dist” that takes in two
-		double variables(mean and standard deviation), and returns back a
-		random number from a normal distribution.*/
+public:
+	Plane(const string& from, const string& to)
+		: wait_time(0.0), pos(0.0), vel(0.0), distance(0.0), loiter_time(0.0), at_SCE(false), origin(from), destination(to) {
+	
+	}
+
+	virtual ~Plane(){
+	
+	}
+
+	void operate(double dt){
+
+	}
+
+	double getPos() const {
+		return pos;
+	}
+
+	double getVel() const {
+		return vel;
+	}
+
+	double get_loiter_time() const {
+		return loiter_time;
+	}
+
+	string getOrigin() const {
+		return origin;
+	}
+
+	string getDestination() const {
+		return destination;
+	}
+
+	bool getAtSCE() const {
+		return at_SCE;
+	}
+
+	void setVel(double vel2) {
+		vel = vel2;
+	}
+
+	void setLoiterTime(double loiter_time2) {
+		loiter_time = loiter_time2;
+	}
+
+	double distance_to_SCE() {
+		return distance;
+	}
+
+	virtual double time_on_ground() {
+		return 0.0;
+	}
+
+	virtual string plane_type() {
+		return "String";
+	}
+
+	static double draw_from_normal_dist(double mean, double std_dev) {
+		// Implementation for the "draw_from_normal_dist" function
+		default_random_engine rand_generator;
+		normal_distribution<double> distribution(mean, std_dev);
+		return distribution(rand_generator);
+
 };
+
+class Q3
+{
+
+}
 
 int main()
 {
@@ -62,5 +113,6 @@ int main()
 
 	string sourceAirport = "SCE";
 	string destinationAirport = "PHL";
+
 	return 0;
 }
